b45a0110d1a6f1ce0b487701f6ecacae16d5f8fd
John-Hanekom <113033854+John-Hanekom@users.noreply.github.com>
2022-09-27 09:21:21 -0400

    Added the downloaded code

---
+++ Temerature.py
@@ -0,0 +1,13 @@
+# Created by: John Hanekom
+# Date: September 26th, 2022
+# 
+# This program says the temperature every second
+
+def on_forever do
+    global current_temperature
+    current_temperature = input.temperature()
+    basic.show_number(current-temperature)
+    basic.pause(1000)
+    basic.clear_screen()
+basic.forever(on_forever)
+
